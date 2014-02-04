31ea854b054029634fadd554181059cd3b904a42
Nicholas Hannah <nicholas.hannah@unsw.edu.au>
2014-02-04 15:59:57 +1100

    Move build and compiler scripts, add test script.

---
+++ test/test.py
@@ -0,0 +1,19 @@
+
+import unittest
+
+class TestBasicRun(unittest.TestCase):
+
+    def test_build(self):
+        """
+        Build all auscom models. 
+        """
+
+        pass
+
+    def test_run(self):
+        """
+        Do a one month low res run and check the output. 
+        """
+
+if __name__ == '__main__':
+    unittest.main()
