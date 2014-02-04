2fe7a9ec486aba1d8217d68c8c8ec5df6c6a48df
Nicholas Hannah <nicholas.hannah@unsw.edu.au>
2014-02-04 12:44:04 +1100

    Run script for 1 degree gfortran on desktop.

--- exp/cnyf2.mom5.ubuntu/qsub_run.sh
+++ exp/cnyf2.mom5.ubuntu/qsub_run.sh
@@ -10,8 +10,8 @@
 #PBS -o OCN_RUNDIR/stdout.txt
 #PBS -e OCN_RUNDIR/stderr.txt
 
-#module load openmpi
-#module load ipm
+module load openmpi/1.6.5-mlx
+module load ipm
 
-mpirun --mca orte_base_help_aggregate 0 -wdir /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/ATM_RUNDIR -n 1 /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/matmxx : -wdir /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/ICE_RUNDIR -n 1 /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/cicexx : -wdir /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/OCN_RUNDIR -n 4 /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/mom5xx 2> stderr.txt 1> stdout.txt
+mpirun --mca orte_base_help_aggregate 0 -wdir /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/ATM_RUNDIR -n 1 /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/matmxx : -wdir /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/ICE_RUNDIR -n 1 /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/cicexx : -wdir /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/OCN_RUNDIR -n 4 /home/nfs/z3078140/auscom/exp/cnyf2.mom5.ubuntu/mom5xx
 
