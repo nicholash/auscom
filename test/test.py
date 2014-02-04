a744c6d4abcd8cffc2b32658e0f1b33cbe1cb1d6
Nicholas Hannah <nicholas.hannah@unsw.edu.au>
2014-02-04 16:08:48 +1100

    Auscom build and run tests.

--- test/test.py
+++ test/test.py
@@ -1,19 +1,26 @@
 
 import unittest
+import subprocess
 
-class TestBasicRun(unittest.TestCase):
+class TestBasic(unittest.TestCase):
 
     def test_build(self):
         """
         Build all auscom models. 
         """
-
-        pass
+        
+        ret = subprocess.call(['../bin/clean_auscom.sh', 'nci'], shell=True)
+        self.assertEqual(ret, 0)
+        ret = subprocess.call(['../bin/build_auscom.sh', 'nci'], shell=True)
+        self.assertEqual(ret, 0)
 
     def test_run(self):
         """
-        Do a one month low res run and check the output. 
+        Do a one month low res run and check for successful completion. 
         """
+
+        ret = subprocess.call(['../bin/run.py', '--newrun', '--rundirect', '--submitruntime', '1', 'cnyf2.mom5'])
+        self.assertEqual(ret, 0)
 
 if __name__ == '__main__':
     unittest.main()
